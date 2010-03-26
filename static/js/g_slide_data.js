c1a03da8bfb4a132e6517ccc02050a42e6645acb
Jon Lesser <jon.lesser@climateprotect.org>
2010-03-26 09:53:50 -0400

    merging changes from charles Adding site credits Adding hover state Moving g_slide_data into it's own js file

---
+++ static/js/g_slide_data.js
@@ -0,0 +1,187 @@
+var g_slide_data = [
+	{
+        title: "Hubble Telescope in Baltimore",
+        deck: "Baltimore lights the path to the future of astronomy ",
+        thumbnail: {
+            src: "/static/images/wfl/img_hubble_thumb.jpg",
+            alt: "Hubble",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/hubble.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_hubble_01.jpg",
+                alt: "Hubble",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Open Conduit System",
+        deck: "A comprehensive city-owned conduit system offers a unique opportunity",
+        thumbnail: {
+            src: "/static/images/wfl/img_conduits_thumb.jpg",
+            alt: "Conduits",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/conduits.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_conduits_01.jpg",
+                alt: "Conduits",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Walters Manuscripts",
+        deck: "Sharing ancient writings with the world through Google fiber",
+        thumbnail: {
+            src: "/static/images/wfl/img_walters_thumb.jpg",
+            alt: "Serious Games",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/walters.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_walters_01.jpg",
+                alt: "Serious Games",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Telesurgery",
+        deck: "Extending surgical care through robotics",
+        thumbnail: {
+            src: "/static/images/wfl/img_telesurgery_thumb.jpg",
+            alt: "Telesurgery thumbnail",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/telesurgery.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_telesurgery_01.jpg",
+                alt: "Telesurgery",
+                width: "2000",
+                height: "1333"
+            }
+            // Uncomment these lines to load additional images into a slide. Transitioning between images is still a todo.
+            // ,
+            // {
+            //     src: "/static/images/wfl/img_telesurgery_01.jpg",
+            //     alt: "Walters",
+            //     width: "2000",
+            //     height: "1333"
+            // },
+            // {
+            //     src: "/static/images/wfl/img_walters_01.jpg",
+            //     alt: "Walters",
+            //     width: "2000",
+            //     height: "1333"
+            // }
+        ]
+    },
+	{
+        title: "Cybersecurity",
+        deck: "Leading the way to a safer world",
+        thumbnail: {
+            src: "/static/images/wfl/img_cybersecurity_thumb.jpg",
+            alt: "Cybersecurity",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/cybersecurity.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_cybersecurity_01.jpg",
+                alt: "Cybersecurity",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Baltimore Lightning Cloud",
+        deck: "Applications for a cloud the size of Charm City",
+        thumbnail: {
+            src: "/static/images/wfl/img_lightning_cloud_thumb.jpg",
+            alt: "cloud",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/lightning_cloud.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_lightning_cloud_01.jpg",
+                alt: "Cloud",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Serious Games",
+        deck: "Immersive Learning and Interactive Simulations",
+        thumbnail: {
+            src: "/static/images/wfl/img_breakaway_thumb.jpg",
+            alt: "Serious Games",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/serious_games.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_breakaway_01.jpg",
+                alt: "Serious Games",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Indie Music Hotbed",
+        deck: "Musical Collaboration at the Speed of Light",
+        thumbnail: {
+            src: "/static/images/wfl/img_music_thumb.jpg",
+            alt: "Music",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/music.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_music_02.jpg",
+                alt: "Music",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    },
+	{
+        title: "Medical Imaging",
+        deck: "Improving Healthcare through Data-intensive Electronic Health Records",
+        thumbnail: {
+            src: "/static/images/wfl/img_medical_images_thumb.jpg",
+            alt: "Teleradiology",
+            width: "208",
+            height: "139"
+        },
+        content: "/static/html/teleradiology.html",
+        images: [
+            {
+                src: "/static/images/wfl/img_medical_images_01.jpg",
+                alt: "Teleradiology",
+                width: "2000",
+                height: "1333"
+            }
+        ]
+    }
+];
